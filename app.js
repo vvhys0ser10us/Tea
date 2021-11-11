1e7b2b65df0363a9a5e555f4da52f454c0b9ebbe
Haoran Zhou <kens0serious@gmail.com>
2021-11-11 02:09:54 -0500

    format code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 // setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
@@ -14,4 +13,3 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
-
